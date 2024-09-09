f901ecca100391e5c5540e9cf755f2da959e5fd4
Dauren Shalabayev <dauren10@yandex.kz>
2024-09-10 00:31:12 +0500

    Create longest-common-prefix.go

---
+++ longest-common-prefix.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	arr := []string{"flower", "flow", "flight"}
+
+	longestCommonPrefix(arr)
+}
+
+func longestCommonPrefix(strs []string) string {
+	var temp string
+	var max string
+	for i := 0; i < len(strs); i++ {
+		for j := 0; j < len(strs[i]); j++ {
+			if strs[i][j]
+		}
+	}
+	return "err"
+}

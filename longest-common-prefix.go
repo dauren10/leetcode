66adb95348c55b086f886b06f95717d1a8ed8e87
OEM Configuration (temporary user) <d.shalabayev@kazdream.kz>
2024-09-25 12:13:24 +0500

    longest prefix

--- longest-common-prefix.go
+++ longest-common-prefix.go
@@ -1,18 +1,38 @@
 package main
 
-func main() {
-	arr := []string{"flower", "flow", "flight"}
-
-	longestCommonPrefix(arr)
-}
+import (
+	"fmt"
+)
 
 func longestCommonPrefix(strs []string) string {
-	var temp string
-	var max string
-	for i := 0; i < len(strs); i++ {
-		for j := 0; j < len(strs[i]); j++ {
-			if strs[i][j]
+	if len(strs) == 0 {
+		return ""
+	}
+
+	// Берем первую строку в качестве начального префикса
+	prefix := strs[0]
+
+	// Проходим по всем остальным строкам
+	for _, s := range strs[1:] {
+		// Пока текущая строка не содержит префикс, сокращаем префикс
+		for len(prefix) > 0 && len(s) < len(prefix) || s[:len(prefix)] != prefix {
+			prefix = prefix[:len(prefix)-1]
+		}
+		// Если префикс стал пустым, выходим
+		if prefix == "" {
+			return ""
 		}
 	}
-	return "err"
+
+	return prefix
+}
+
+func main() {
+	// Пример 1
+	strs1 := []string{"flower", "flow", "flight"}
+	fmt.Println(longestCommonPrefix(strs1)) // "fl"
+
+	// Пример 2
+	strs2 := []string{"dog", "racecar", "car"}
+	fmt.Println(longestCommonPrefix(strs2)) // ""
 }
